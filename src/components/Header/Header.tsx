6a60b509b0c568624dd99fd3ca489dc4bb0998f1
GerardoRosas <rosas.ruizlg@gmail.com>
2023-06-08 18:34:41 -0600

    Second commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+
+
+
+export default function Header() {
+    return ( 
+        <>
+            <header className="text-2xl font-bold underline">
+                <div>
+                    <small>4 Esquinas image</small>
+                </div>
+
+                <div>
+                    
+                </div>
+            </header>
+        </>
+    )   
+}
